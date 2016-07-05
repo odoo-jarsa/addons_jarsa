b76c6cc62823d746f74ebc28ed3cf1f5abb4e1f1
Lázaro <lazaro@quadit.mx>
2016-07-05 17:32:57 -0500

    Añadí wrapper al reporte de etiquetas

--- mrp_workflow_print_label/wizard/mrp_print_label.py
+++ mrp_workflow_print_label/wizard/mrp_print_label.py
@@ -1,4 +1,4 @@
-from openerp import fields, models
+from openerp import fields, models, api
 
 
 class MrpPrintLabel(models.TransientModel):
@@ -27,14 +27,12 @@ class MrpPrintLabel(models.TransientModel):
     def print_report(self, cr, uid, ids, context=None):
         if context is None:
             context = {}
-        data = self.read(cr, uid, ids)[0]
-        datas = {
-            'ids': context.get('active_ids', []),
-            'model': 'mrp.production',
-            'form': data
-        }
-        datas['form']['active_ids'] = context.get('active_ids', False)
-
+        datas = {'ids': context.get('active_ids', [])}
+        res = self.read(cr, uid, ids, ['lote_impresion'], context=context)
+        res = res and res[0] or {}
+        datas['form'] = res
+        if res.get('id',False):
+            datas['ids'] = [res['id']]
         return self.pool['report'].get_action(
             cr, uid, [],
             'mrp_workflow_print_label.label_qweb',
