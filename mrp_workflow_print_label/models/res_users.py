8af8eb548bdc3a28d158ade2c6838b4f8ff95cde
Alan Ramos <alan.ramos@jarsa.com.mx>
2016-07-06 21:18:13 -0500

    [ADD] sql constraint to PIN in res.users

--- mrp_workflow_print_label/models/res_users.py
+++ mrp_workflow_print_label/models/res_users.py
@@ -2,36 +2,17 @@
 # © 2016 Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import _, fields, models
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
+    _name = 'res.users'
 
-    mrp_pin = fields.Integer(
+    mrp_pin = fields.Char(
         string='PIN',
     )
 
-    # def _check_unique_pin(self, cr, uid, ids, context=None):
-
-    #     record = self.browse(cr, uid, ids, context=context)
-    #     pin_codes = []
-
-    #     for data in record:
-
-    #         if data.mrp_pin > 999:
-    #             pin_codes = self.search(
-    #                 cr, uid, [(
-    #                     'mrp_pin', '=', data.mrp_pin), ('id', '!=', data.id)])
-
-    #             if len(pin_codes) > 0:
-    #                 return False
-    #             else:
-    #                 return True
-    #         else:
-    #             return True
-
-    # _constraints = [(
-    #     _check_unique_pin,
-    #     'Error: This PIN code is invalid or assigned to other user.', [
-    #         'mrp_pin'])]
+    _sql_constraints = [(
+        'mrp_pin_unique', 'UNIQUE(mrp_pin)', _("The PIN must be unique")),
+    ]
