91fb5099a41e0b0a7e07529d45f5220e7e584b82
Alan Ramos <alan.ramos@jarsa.com.mx>
2016-07-08 09:24:06 -0500

    [FIX] Merge conflicts

--- mrp_workflow_print_label/wizard/mrp_print_label_validate.py
+++ mrp_workflow_print_label/wizard/mrp_print_label_validate.py
@@ -34,8 +34,5 @@ class MrpPrintLabelValidate(models.TransientModel):
                 'type': 'ir.actions.report.xml',
                 'report_name': report_name,
                 'context': context,
-<<<<<<< Updated upstream
                 'docs': self.order_id.id
-=======
->>>>>>> Stashed changes
             }
