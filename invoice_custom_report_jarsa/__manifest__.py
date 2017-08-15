237953e5012b8d5477dbaff27b1cf2c92e3899d2
IsabelEsparza <isa_tkm_ris@live.com.mx>
2017-08-15 17:29:58 -0500

    [ADD][invoice_custom_report_jarsa]Add a new module for Jarsa's invoice report

---
+++ invoice_custom_report_jarsa/__manifest__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2017, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Custom Invoice Report",
+    "version": "10.0.0.1.0",
+    "category": "Report",
+    "author": "Jarsa Sistemas",
+    "depends": [
+        "sale",
+    ],
+    "data": [
+        'report/l10n_mx_base_report.xml',
+    ],
+    "installable": True,
+}
