edb674904991c71c246fcef508e3d630e1583aeb
SaraiOsorio <sarai.osorio@jarsa.com.mx>
2017-12-08 17:56:48 -0600

    [REF][crm_new_lead]return view

--- crm_new_lead/__manifest__.py
+++ crm_new_lead/__manifest__.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 # Copyright 2017, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,10 +5,13 @@
 {
     'name': 'Create New Lead',
     'version': '10.0.0.1.0',
-    'category': 'Initial Data',
+    'category': 'Sales',
     'author': 'Jarsa Sistemas',
     'description': 'Create new lead from ticket',
-    'depends': ['crm', 'helpdesk'],
+    'depends': [
+        'crm',
+        'helpdesk'
+    ],
     'data': [
         'views/crm_lead.xml',
     ],
--- crm_new_lead/models/helpdesk_ticket.py
+++ crm_new_lead/models/helpdesk_ticket.py
@@ -10,15 +10,13 @@ class HelpdeskTicket(models.Model):
 
     @api.multi
     def new_crm_lead(self):
+        self.ensure_one()
         obj_crm = self.env['crm.lead']
-        partner = (
-            self.partner_id.parent_id.id
-            if self.partner_id.parent_id
-            else self.partner_id.id)
+        partner = self.partner_id.commercial_partner_id.id
         contact_name = self.partner_id.name
         title = self.partner_id.title.id if self.partner_id.title else False
 
-        obj_crm.create({
+        lead = obj_crm.create({
             'name': self.name,
             'partner_id': partner,
             'team_id': 1,
@@ -31,3 +29,16 @@ class HelpdeskTicket(models.Model):
         stage = self.env['helpdesk.stage'].search(
             [('is_close', '=', True)])
         self.stage_id = stage[-1].id
+        self.active = False
+
+        return {
+            'name': 'New lead',
+            'view_id': self.env.ref(
+                'crm.crm_case_form_view_leads').id,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'current',
+            'res_model': 'crm.lead',
+            'res_id': lead.id,
+            'type': 'ir.actions.act_window'
+        }
